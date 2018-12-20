a113f7196aeeae17888de2a3831d1a65d157bb9a
Federico Pardo <fpg23feb98@gmail.com>
2018-12-20 13:43:59 +0100

    main.cc commented

--- src/main.cc
+++ src/main.cc
@@ -17,6 +17,7 @@ using namespace std;
 ofstream f1;
 ofstream f2;
 
+// Returns false if the file cannot be opened. True otherwise
 bool CheckFile(ifstream &file, const char *file_name)
 {
   if (!file)
@@ -30,13 +31,18 @@ bool CheckFile(ifstream &file, const char *file_name)
 
 int main(int argc, char const *argv[])
 {
+  // Stream for the 3 files
   ifstream file_kb(argv[1]);
   ifstream config_file(argv[2]);
   ifstream file_facts(argv[3]);
+
+  // Sets the name of the exit files
   string st1(argv[3]);
-  st1 = st1.substr(st1.size()-6, st1.size());
+  st1 = st1.substr(st1.size() - 6, st1.size());
   string concat1 = "Salida1-" + st1;
   string concat2 = "Salida2-" + st1;
+
+  // Opens the files to write in them
   f1.open(concat1);
   f2.open(concat2);
 
@@ -46,19 +52,24 @@ int main(int argc, char const *argv[])
     if (argv[i] == NULL || !CheckFile(file_kb, argv[i]))
     {
       cerr << "Arguments are not correct." << endl
-           << "Use: ./sbr KnowledgeBase.txt ConfigFile.txt FactsFile.txt" << endl
+           << "Usage: ./sbr KnowledgeBase.txt ConfigFile.txt FactsFile.txt" << endl
            << "Stop." << endl;
       exit(1);
     }
   }
 
+  // Creates the 3 objects used for the 3 files
   Config *c = new Config(config_file);
   KnowledgeBase *kb = new KnowledgeBase(file_kb, c);
   FactsBase *fb = new FactsBase(file_facts);
 
+  // Calls the inference engine and start processing the solution
   InferenceEngine::ForwardChaining(*kb, *c, *fb);
 
+  // Delete the objects and all his allocated memory
   delete kb;
   delete fb;
+
+  // Program succeded!
   return 0;
 }
